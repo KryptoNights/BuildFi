dd49af96d33a1f06424930e392695c728f4ad61c
debjit <debjitbhowal.db@gmail.com>
2024-04-10 22:10:36 +0530

    oidc validator for linkedin

---
+++ zk-kyc/zkvm/oidc-validator/src/certs.rs
@@ -0,0 +1,17 @@
+// Copyright 2024 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+pub static LINKEDIN_JWKS: &str = r#"
+{"keys":[{"kty":"RSA","e":"AQAB","kid":"d929668a-bab1-4c69-9598-4373149723ff","n":"zm2NAknfJAdRtrFw7xDmxcO0STPFDkLeZMvi3T7nP7QsNlQUjGxZoYuocpVzGpB_ZssIfKHuzOEMYZh40VD7JcU6dX-6sBH9y1V45TzT2_XvpfGcjM-5snlAF-wMN6Sl1zf4eapyqHjixkxh8in5LAE1vyfmGkgtcRabadoU6nmqUr-smTWL-8hPWxqP2GkVmzSZ2WRrxLtxXIERMgT8MddhHsT4bE51TOWN8egdpcMngGYS2kjlp6BLVXgBmypM0KRb_pRrWCJaVXPTl7z_esqUvqs0BYN1Z6whl7ncHZgUUmf3i2SqyZy0btpyrh-lZnxM0wx9Sb1gOLRkByREiHLNy4IzLqwxv0pxRsJLQah4BKezOFcO6VXWVEO0HGZhRN-SHhLBsrHiybcMlxEODczKB4-nqDLog6xjtFJTj_LndDJwJpiZsSTWUwp0-PzbCayl8s0h--U1SPSr1ux-A9ScHUXOVfYr0wL4gfOtrx-XfD0SrO_qADb3lZ94aBastesh5_Ha4abqtltGmdDkmMG90WdaGvICsdv0c2VtrZ8QUIaOv0yjDWVocR3PEr8j3VQn3ox-LdGkPU8X-ZdQdyWytQBUtsT-e5tZqxHR8HSLcHAG60Kg0nCTE5UP6rbEyTRd2-t_ST33qkUohcn64kXl8SNB30lY1fF1_LKtFwc"}]}
+"#;
