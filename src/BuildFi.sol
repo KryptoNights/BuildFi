fa5b917866e3c390b39fffa98208dabbe7d95ff7
debjit <debjitbhowal.db@gmail.com>
2024-04-12 15:39:06 +0530

    changes

--- src/BuildFi.sol
+++ src/BuildFi.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 // a crowd-investment platform where developers can raise funds for their projects from day 1. it's milestone based and the funds get disbursed on successful milestone completion
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract BuildFi {
     struct Developer {
@@ -19,6 +19,7 @@ contract BuildFi {
     mapping (address => Developer) public developers;
 
     struct Project {
+        uint256 id;
         string name;
         string project_metadata_json;
 
@@ -31,6 +32,24 @@ contract BuildFi {
         // participants
         address[] investors;
         mapping (address => uint256) investments;
+
+        // payouts
+        address projectToken;
+        bool token_set;
     }
-    mapping (address => Project) public projects;
+    mapping (uint256 => Project) public projects;
+
+    uint256 public projectCount;
+
+    constructor() {
+        projectCount = 0;
+    }
+
+    function makeNewAccount(string memory _name, string memory _email, string memory _zkKYC_proof_id) public {
+        // ensure the developer doesn't already exist
+        require(bytes(developers[msg.sender].name).length == 0, "Developer already exists");
+
+        // create a new developer account
+        developers[msg.sender] = Developer(_name, _email, _zkKYC_proof_id, 0, 0);
+    }
 }
