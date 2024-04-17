f24c82a13697919d3bd30b5aff0ab7c8edb13bb2
Kavish Shah <kavishshah30@gmail.com>
2024-04-17 13:01:34 +0530

    project slug added

--- frontend_buildfi/components/WrapperCard/Card.tsx
+++ frontend_buildfi/components/WrapperCard/Card.tsx
@@ -2,11 +2,24 @@ import React from "react";
 import styles from "./cards.module.css";
 import BitCoin from "public/bitcoin.webp";
 import Image from "next/image";
-const Card = ({ item }: { item: any }) => {
+
+const Card = ({
+  item,
+  key,
+  handleClick,
+}: {
+  item: any;
+  key: number;
+  handleClick: any;
+}) => {
+  const handleClickWrapper = () => {
+    handleClick(item.id);
+  };
+
   return (
-    <div className={styles.wrapperCard}>
+    <div className={styles.wrapperCard} onClick={handleClickWrapper}>
       <div className={styles.card}>
-        <Image src={BitCoin} alt={'coin'} width={395} height={280}/>
+        <Image src={BitCoin} alt={"coin"} width={395} height={280} />
       </div>
       <div className={styles.txt1}>{item.name}</div>
       <div className={styles.txt2}>{item.description}</div>
