685d063dc85a8ae67b1574f70ac88127226caca1
Akshay Sharma <codewithakki@gmail.com>
2024-04-17 12:31:04 +0530

    Projects

--- frontend_buildfi/components/WrapperCard/Card.tsx
+++ frontend_buildfi/components/WrapperCard/Card.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import styles from "./cards.module.css";
+import BitCoin from "public/bitcoin.webp";
+import Image from "next/image";
 const Card = ({ item }: { item: any }) => {
   return (
     <div className={styles.wrapperCard}>
-      <div className={styles.card}>{item.name}</div>
+      <div className={styles.card}>
+        <Image src={BitCoin} alt={'coin'} width={395} height={280}/>
+      </div>
       <div className={styles.txt1}>{item.name}</div>
       <div className={styles.txt2}>{item.description}</div>
     </div>
