51946b8446c7024019144364411155f6e72f480e
Akshay Sharma <codewithakki@gmail.com>
2024-04-19 15:02:56 +0530

    bugs

--- frontend_buildfi/components/ProjectCreate/ProjectCreate.tsx
+++ frontend_buildfi/components/ProjectCreate/ProjectCreate.tsx
@@ -1,28 +1,32 @@
+import useConnection from "@/utils/useConnection";
 import { useState } from "react";
-
+import { createProject } from "@/utils/transitions";
 const ProjectCreate = () => {
+  const { signer } = useConnection();
   const [metadata, setMetadata] = useState("");
   const [name, setName] = useState("");
-  const [totalBudget, setTotalBudget] = useState("");
+  const [image, setBanner] = useState("");
+  const [description, setDiscription] = useState("");
+  const [totalBudget, setTotalBudget]: any = useState(null);
   const [fundingEndDate, setFundingEndDate] = useState("");
-  const [numMilestones, setNumMilestones] = useState("");
+  const [numMilestones, setNumMilestones]: any = useState(null);
   const [milestones, setMilestones]: any = useState([]);
-  const convertDateToMs=(dateString:any)=> {
+  const convertDateToMs = (dateString: any) => {
     const date = new Date(dateString);
     const ms = date.getTime();
-    return ms*1000;
-  }
-  
-  const milestonesWithIsoDate = milestones.map((milestone:any )=> {
+    return ms * 1000;
+  };
+
+  const milestonesWithIsoDate = milestones.map((milestone: any) => {
     return {
-      date:  convertDateToMs(milestone.date),
-      fundPercentage: milestone.fundPercentage
+      date: convertDateToMs(milestone.date),
+      fundPercentage: milestone.fundPercentage,
     };
   });
   const handleSubmit = (event: any) => {
     event.preventDefault();
     console.log("ff", fundingEndDate);
-    const date= convertDateToMs(fundingEndDate);
+    const date = convertDateToMs(fundingEndDate);
     console.log("ff", milestonesWithIsoDate);
 
     // Add your form submission logic here
@@ -38,34 +42,59 @@ const ProjectCreate = () => {
     setMilestones([...milestones, { date: "", fundPercentage: "" }]);
   };
 
+  const handlecreateProject = async () => {
+    const datesArray:any = [];
+    const fundPercentagesArray:any = [];
+   await  milestonesWithIsoDate.forEach((milestone: any) => {
+      datesArray.push(milestone.date);
+      fundPercentagesArray.push(milestone.fundPercentage);
+    });
+    console.log('name',name);
+    console.log('image',image);
+    console.log('description',description);
+    console.log('metadata',metadata);
+    console.log('endDate',convertDateToMs(fundingEndDate));
+    console.log('fundPercentagesArray',fundPercentagesArray);
+    console.log('datesArray',datesArray);
+
+    return; 
+
+    console.log();
+    try {
+      await createProject(
+        name,
+        image,
+        description,
+        metadata,
+        numMilestones,
+        milestones,
+        totalBudget,
+        convertDateToMs(fundingEndDate),
+        signer!
+      );
+      console.log("okay");
+    } catch (error) {
+      console.error("Error investing in project:", error);
+    }
+  };
+
+  // description: string,
+  // metadata: string,
+  // milestones: number,
+  // payoutPercentages: number[],
+
   return (
     <div className="m-auto w-full max-w-2xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-      <form className="space-y-6" onSubmit={handleSubmit}>
+      <form className="space-y-6">
         <h5 className="text-xl font-bold  text-gray-900 dark:text-white">
           Project Details
         </h5>
         <div>
           <label
-            htmlFor="metadata"
-            className="block mb-4 text-l font-medium text-gray-900 dark:text-white"
-          >
-            Metadata (Image Link)
-          </label>
-          <input
-            type="text"
-            id="metadata"
-            value={metadata}
-            onChange={(e) => setMetadata(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-l rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-            required
-          />
-        </div>
-        <div>
-          <label
             htmlFor="name"
             className="block mb-4  text-l font-medium text-gray-900 dark:text-white"
           >
-            Name of project
+            Name of Project
           </label>
           <input
             type="text"
@@ -78,13 +107,61 @@ const ProjectCreate = () => {
         </div>
         <div>
           <label
+            htmlFor="name"
+            className="block mb-4  text-l font-medium text-gray-900 dark:text-white"
+          >
+            Upload Banner of Project
+          </label>
+          <input
+            type="text"
+            id="name"
+            value={image}
+            onChange={(e) => setBanner(e.target.value)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="name"
+            className="block mb-4  text-l font-medium text-gray-900 dark:text-white"
+          >
+            Description of Project
+          </label>
+          <textarea
+            id="name"
+            value={description}
+            onChange={(e) => setDiscription(e.target.value)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="metadata"
+            className="block mb-4 text-l font-medium text-gray-900 dark:text-white"
+          >
+            Metadata (Addition Information)
+          </label>
+          <input
+            type="text"
+            id="metadata"
+            value={metadata}
+            onChange={(e) => setMetadata(e.target.value)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-l rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            required
+          />
+        </div>
+
+        <div>
+          <label
             htmlFor="totalBudget"
             className="block mb-4  text-l font-medium text-gray-900 dark:text-white"
           >
             Total Budget in Etherium
           </label>
           <input
-            type="text"
+            type="number"
             id="totalBudget"
             value={totalBudget}
             onChange={(e) => setTotalBudget(e.target.value)}
@@ -133,41 +210,50 @@ const ProjectCreate = () => {
           />
         </div>
         {milestones.map((milestone: any, index: any) => (
-          <div key={index} className="mb-4 ">
-            <label
-              htmlFor={`milestoneDate${index}`}
-              className="block mb-4 text-l font-medium text-gray-900 dark:text-white"
-            >
-              Milestone {index + 1} Date
-            </label>
-            <input
-              type="date"
-              id={`milestoneDate${index}`}
-              value={milestone.date}
-              onChange={(e) =>
-                handleMilestoneChange(index, "date", e.target.value)
-              }
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-l rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-              required
-            />
-            <label
-              htmlFor={`milestoneFundPercentage${index}`}
-              className="block mb-2 mt-4 text-l font-medium text-gray-900 dark:text-white"
-            >
-              Milestone {index + 1} Fund Percentage
-            </label>
-            <input
-              type="text"
-              id={`milestoneFundPercentage${index}`}
-              value={milestone?.fundPercentage}
-              onChange={(e) =>
-                handleMilestoneChange(index, "fundPercentage", e.target.value)
-              }
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-              required
-            />
+          <div key={index} className="mb-4 flex-col">
+            {" "}
+            <div className="mb-4 flex-col">
+              {" "}
+              <label
+                htmlFor={`milestoneDate${index}`}
+                className="block mb-4 text-l font-medium text-gray-900 dark:text-white"
+              >
+                Milestone {index + 1} Date
+              </label>
+              <input
+                type="date"
+                id={`milestoneDate${index}`}
+                value={milestone.date}
+                onChange={(e) =>
+                  handleMilestoneChange(index, "date", e.target.value)
+                }
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-l rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                required
+              />
+            </div>
+            <div>
+              {" "}
+              {/* Remove class flex-col */}
+              <label
+                htmlFor={`milestoneFundPercentage${index}`}
+                className="block mb-2 mt-4 text-l font-medium text-gray-900 dark:text-white"
+              >
+                Milestone {index + 1} Fund Percentage
+              </label>
+              <input
+                type="text"
+                id={`milestoneFundPercentage${index}`}
+                value={milestone?.fundPercentage}
+                onChange={(e) =>
+                  handleMilestoneChange(index, "fundPercentage", e.target.value)
+                }
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                required
+              />
+            </div>
           </div>
         ))}
+
         <button
           type="button"
           onClick={addMilestone}
@@ -176,7 +262,7 @@ const ProjectCreate = () => {
           Add Milestone
         </button>
         <button
-          type="submit"
+          onClick={handlecreateProject}
           className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Create Project
