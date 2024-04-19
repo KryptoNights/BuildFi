ce3c8cd0ff326412654c2cd118fdcce7270efe6d
Kavish Shah <kavishshah30@gmail.com>
2024-04-19 10:40:29 +0530

    investments

---
+++ frontend_buildfi/src/pages/investments/index.tsx
@@ -0,0 +1,41 @@
+import { useRouter } from "next/router";
+import WrapperCards from "../../../components/WrapperCard/WrapperCards";
+import { useEffect, useState } from "react";
+import { getAllProjects } from "@/utils/transitions";
+
+const index = () => {
+  const router = useRouter();
+
+  const [projects, setProjects] = useState<Project[]>([]);
+  console.log(projects);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const projectsData = await getAllProjects();
+        setProjects(projectsData);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleClick = async (id: number) => {
+    const project = projects.find((item) => item.id === id);
+    if (project) {
+      router.push(`/projects/${project.id}`);
+      console.log("id", project.id);
+    }
+  };
+  return (
+    <>
+      <div className="flex flex-row w-full m-auto z-999 justify-between">
+        <WrapperCards data={projects} handleClick={handleClick} />
+      </div>
+    </>
+  );
+};
+
+export default index;
